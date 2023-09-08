944be124156413ba4ebd4f7aa2c4d9b4d198b420
infotech87871 <144329303+infotech87871@users.noreply.github.com>
2023-09-08 11:50:12 +0530

    Create Buy.java

---
+++ Buy.java
@@ -0,0 +1,14 @@
+byte companyAddress = 50;
+		System.out.println("Company Age : "+companyAddress);
+		
+		short companyId = 1616;
+		System.out.println("Company Id : "+companyId);
+		
+		int companyCode = 4387545;
+		System.out.println("Company Code : "+companyCode);
+		
+		long companyNumber = 7538874657l;
+		System.out.println("Company Number : "+companyNumber);
+		
+		float minimumSalary = 80000.3434454f;
+		System.out.println("Minimum Salary : "+minimumSalary);
